cf87de9cbdf64475668eb1be307f90b98c45758e
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-08-25 22:30:07 -0400

    local storage button

---
+++ light.js
@@ -0,0 +1,25 @@
+var localStorage = window.localStorage;
+var colorMode;
+
+$(document).ready(function () {
+  colorMode = document.documentElement.dataset.theme;
+});
+
+function toggleColorMode() {
+  if (colorMode === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    updateColorPreference('dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    updateColorPreference('light');
+  }
+}
+
+function updateColorPreference(mode) {
+  colorMode = mode;
+  saveColorPreference(colorMode);
+}
+
+function saveColorPreference(mode) {
+  localStorage.setItem('mode', mode);
+}
