024ecff0234c12a8d760fc39ea0275ca9be03e6e
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-09-17 14:04:38 -0400

    stying for mobile

--- website/src/components/About.js
+++ website/src/components/About.js
@@ -25,57 +25,57 @@ const About = () => {
           <div>
             <div>
               <i className='devicon devicon-mongodb-plain colored'></i>
-              <span>MongoDB</span>
+              <span className='devicon-label'>MongoDB</span>
             </div>
             <div>
-              <i className='devicon devicon-mongodb-plain colored'></i>
-              <span>MongoDB</span>
+              <i className='devicon devicon-python-plain colored'></i>
+              <span className='devicon-label'>Python</span>
             </div>
+            <div>
+              <i className='devicon devicon-html5-plain colored'></i>
+              <span className='devicon-label'>HTML</span>
+            </div>
+          </div>
+          <div>
             <div>
               <i className='devicon devicon-express-original colored'></i>
-              <span>Express</span>
+              <span className='devicon-label'>Express</span>
+            </div>
+            <div>
+              <i className='devicon devicon-java-plain colored'></i>
+              <span className='devicon-label'>Java</span>
+            </div>
+            <div>
+              <i className='devicon devicon-css3-plain colored'></i>
+              <span className='devicon-label'>CSS</span>
             </div>
           </div>
           <div>
             <div>
-              <i className='devicon devicon-sass-plain colored'></i>
-              <span>Express</span>
+              <i className='devicon devicon-react-original colored'></i>
+              <span className='devicon-label'>React</span>
             </div>
             <div>
-              <i className='devicon devicon-sass-plain colored'></i>
-              <span>Sass</span>
+              <i className='devicon devicon-sass-original colored'></i>
+              <span className='devicon-label'>Sass</span>
             </div>
             <div>
-              <i className='devicon devicon-sass-plain colored'></i>
-              <span>Sass</span>
-            </div>
-          </div>
-          <div>
-            <div>
-              <i className='devicon devicon-sass-plain colored'></i>
-              <span>React</span>
-            </div>
-            <div>
-              <i className='devicon devicon-sass-plain colored'></i>
-              <span>Sass</span>
-            </div>
-            <div>
-              <i className='devicon devicon-bootstrap-plain colored'></i>
-              <span>Bootstrap</span>
+              <i className='devicon devicon-javascript-plain colored'></i>
+              <span className='devicon-label'>JavaScript</span>
             </div>
           </div>
           <div>
             <div>
-              <i className='devicon devicon devicon-github-plain colored'></i>
-              <span>Node.js</span>
+              <i className='devicon devicon-nodejs-plain colored'></i>
+              <span className='devicon-label'>Node.js</span>
             </div>
             <div>
-              <i className='devicon devicon-heroku-original colored'></i>
-              <span>Heroku</span>
+              <i className='devicon devicon-github-plain colored'></i>
+              <span className='devicon-label'>GitHub</span>
             </div>
             <div>
-              <i className='devicon devicon-mongodb-plain colored'></i>
-              <span>MongoDB</span>
+              <i className='devicon devicon-heroku-plain colored'></i>
+              <span className='devicon-label'>Heroku</span>
             </div>
           </div>
         </div>
