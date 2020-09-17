d59c736ed2825d8abc3711d94b44ada98616d4bd
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-09-17 16:11:38 -0400

    Updates

---
+++ precache-manifest.86704efcb0f431f98c9a67b014d3c926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30242191b84383f9ba78a3bbb11598f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855fc6308e937855fc49",
+    "url": "/static/css/main.b7424af6.chunk.css"
+  },
+  {
+    "revision": "ad3da1c7e1790db3d83c",
+    "url": "/static/js/2.82a3a886.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.82a3a886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855fc6308e937855fc49",
+    "url": "/static/js/main.81a42f09.chunk.js"
+  },
+  {
+    "revision": "cac3ef5eed42d856dc75",
+    "url": "/static/js/runtime-main.80f61c10.js"
+  }
+]);
