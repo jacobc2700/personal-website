44dd1243625c1fc9b0267fff4e62fe9e4d5d860a
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-10-23 00:16:59 -0400

    change projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,35 @@ const Projects = () => {
       <section className='projects'>
         <h1 className='project-title'>Projects</h1>
         <div className='card'>
+          <h1>Newton Employee Salaries</h1>
+          <p className='title'>JavaScript</p>
+          <p>Browse payroll data for employees in Newton Public Schools.</p>
+          <div className='card-links'>
+            <a href='http://salaries.thehumboo.com/'>Website</a>
+            <a href='https://youtu.be/eYor5dEHkTI'>Video</a>
+          </div>
+        </div>
+        <div className='card'>
+          <h1>Newton Pavements</h1>
+          <p className='title'>JavaScript</p>
+          <p>Browse street construction data in Newton, Massachusetts.</p>
+          <div className='card-links'>
+            <a href='https://www.thehumboo.com/newton-pavements/'>Website</a>
+            <a href='https://youtu.be/uxe2BDUrhhI'>Video</a>
+          </div>
+        </div>
+        <div className='card'>
+          <h1>COVID-19 Dashboard</h1>
+          <p className='title'>JavaScript</p>
+          <p>
+            Browse virus data for world countries, US counties, and US states.
+          </p>
+          <div className='card-links'>
+            <a href='http://virus.thehumboo.com/'>Website</a>
+            <a href='https://youtu.be/8eJRk9nEbwI'>Video</a>
+          </div>
+        </div>
+        {/* <div className='card'>
           <h1>Flexer</h1>
           <p className='title'>JavaScript</p>
           <p>Flexer is a social media website for aspiring developers.</p>
@@ -31,7 +60,7 @@ const Projects = () => {
             <a href='/#'>Website</a>
             <a href='/#'>GitHub</a>
           </div>
-        </div>
+        </div> */}
       </section>
     </Fragment>
   );
